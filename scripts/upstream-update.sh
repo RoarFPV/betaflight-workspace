35bc7a5d690a98d3ddb724653b1eef4578946e06
RoarFPV <roarfpv@gmail.com>
2022-06-29 15:45:47 -0700

    Update pull order and add some comments to upstream update script

--- scripts/upstream-update.sh
+++ scripts/upstream-update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Script to update "upstream" remotes of submodule branches forked on github
+# *Requires* each submodule to have an "upstream" remote already configured
+
 function updateUpstream () {
     cd $1
     git checkout master && git pull upstream master && git push
@@ -8,9 +11,9 @@ function updateUpstream () {
 }
 
 updateUpstream "betaflight"
+updateUpstream "unified-targets"
 updateUpstream "betaflight-configurator"
 updateUpstream "blackbox-log-viewer"
 updateUpstream "betaflight-tx-lua-scripts"
-updateUpstream "unified-targets"
 
 git status

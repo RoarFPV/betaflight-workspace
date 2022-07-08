809dc16c6dc040441df367023ebe6ca4d7075cb4
RoarFPV <roarfpv@gmail.com>
2022-07-08 01:16:08 -0700

    add remote 'upstream' to update script

--- scripts/upstream-update.sh
+++ scripts/upstream-update.sh
@@ -5,15 +5,17 @@
 
 function updateUpstream () {
     cd $1
+
+    git remote add upstream $2
     git checkout master && git pull upstream master && git push
     cd ../
     git add $1
 }
 
-updateUpstream "betaflight"
-updateUpstream "unified-targets"
-updateUpstream "betaflight-configurator"
-updateUpstream "blackbox-log-viewer"
-updateUpstream "betaflight-tx-lua-scripts"
+updateUpstream "betaflight" "git@github.com:betaflight/betaflight.git"
+updateUpstream "unified-targets" "git@github.com:betaflight/unified-targets.git"
+updateUpstream "betaflight-configurator" "git@github.com:betaflight/betaflight-configurator.git"
+updateUpstream "blackbox-log-viewer" "git@github.com:betaflight/blackbox-log-viewer.git"
+updateUpstream "betaflight-tx-lua-scripts" "git@github.com:betaflight/betaflight-tx-lua-scripts.git"
 
 git status

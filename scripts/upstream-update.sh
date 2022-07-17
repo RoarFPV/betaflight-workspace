081316b7122dfab3f0585175d24626b50508e7ca
RoarFPV <roarfpv@gmail.com>
2022-07-16 20:45:49 -0700

    Update upstreams

--- scripts/upstream-update.sh
+++ scripts/upstream-update.sh
@@ -7,16 +7,16 @@ function updateUpstream () {
     cd $1
 
     git remote add upstream $2
-    git checkout master && git pull upstream master && git push
+    git checkout $3 && git pull upstream $3 && git push
     cd ../
     git add $1
 }
 
-updateUpstream "betaflight" "git@github.com:betaflight/betaflight.git"
-updateUpstream "unified-targets" "git@github.com:betaflight/unified-targets.git"
-updateUpstream "betaflight-configurator" "git@github.com:betaflight/betaflight-configurator.git"
-updateUpstream "blackbox-log-viewer" "git@github.com:betaflight/blackbox-log-viewer.git"
-updateUpstream "betaflight-tx-lua-scripts" "git@github.com:betaflight/betaflight-tx-lua-scripts.git"
-updateUpstream "hdzero/hdzero-vtx" "git@github.com:hd-zero/hdzero-vtx.git"
+updateUpstream "betaflight" "git@github.com:betaflight/betaflight.git" "master"
+updateUpstream "unified-targets" "git@github.com:betaflight/unified-targets.git" "master"
+updateUpstream "betaflight-configurator" "git@github.com:betaflight/betaflight-configurator.git" "master"
+updateUpstream "blackbox-log-viewer" "git@github.com:betaflight/blackbox-log-viewer.git" "master"
+updateUpstream "betaflight-tx-lua-scripts" "git@github.com:betaflight/betaflight-tx-lua-scripts.git" "master"
+updateUpstream "hdzero/hdzero-vtx" "git@github.com:hd-zero/hdzero-vtx.git" "main"
 
 git status

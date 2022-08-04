6ded062025bddc1af1accaede24c17ef77a5b0d7
RoarFPV <roarfpv@gmail.com>
2022-08-03 17:14:21 -0700

    clean up to update script

--- scripts/upstream-update.sh
+++ scripts/upstream-update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Script to update "upstream" remotes of submodule branches forked on github
-# *Requires* each submodule to have an "upstream" remote already configured
 
 function updateUpstream () {
-    cd $1
+    pushd $1
 
     git remote add upstream $2
     git checkout $3 && git pull upstream $3 && git push
-    cd ../
+    popd
     git add $1
 }
 
@@ -20,4 +19,5 @@ updateUpstream "betaflight-tx-lua-scripts" "git@github.com:betaflight/betaflight
 updateUpstream "hdzero/hdzero-vtx" "git@github.com:hd-zero/hdzero-vtx.git" "main"
 updateUpstream "hdzero/hdzero-flasher" "git@github.com:bkleiner/hdzero-flasher.git" "master"
 
+echo "\n\nUpstream pull complete!\n\n"
 git status
